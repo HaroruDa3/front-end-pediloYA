5920618cff71d12b311d850804dbd7e1a096ae4d
HaroruDa3 <harol.kock34@gmail.com>
2023-11-27 23:18:38 -0600

    consumo de la ruta de departamentos
    ya se muestran los departamentos en el select list de departmentos

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/SignIn.css';
 import { useNavigate } from 'react-router-dom';
-import ArrowBack from '../assets/arrow-back.png'
+import ArrowBack from '../assets/arrow-back.png';
+import axios from 'axios';
 
 export const SignIn = () => {
   const navegar = useNavigate();
+  const [departamentos, setDepartamentos] = useState([]);
+
+  useEffect(() => {
+    const obtenerDepartamentos = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/v1/departamentos');
+        setDepartamentos(await response.data);
+      } catch (error) {
+        console.error('Error al obtener departamentos:', error);
+      }
+    };
+
+    obtenerDepartamentos();
+  }, []);
 
   const LogIn = () => {
     navegar('/');
@@ -13,7 +28,6 @@ export const SignIn = () => {
   return (
     <>
       <section id='bk'>
-        
         <div id='div-login'>
           <div id='div-formulario'>
             <div className='d-flex mt-3 mb-3'>
@@ -21,8 +35,8 @@ export const SignIn = () => {
                 <img id='arrow-back' src={ArrowBack} onClick={LogIn} alt="" />
               </div>
               <div className='w-100'>
-                <h3  className='text-center'>Registrarme</h3>
-              </div>  
+                <h3 className='text-center'>Registrarme</h3>
+              </div>
             </div>
             <div className='div-input'>
               <input className='input-login' placeholder='Ingresa tu nombre' type="text" name="nombre" id="nombre" required/>
@@ -47,8 +61,11 @@ export const SignIn = () => {
             <div className='div-input'>
               <select className='input-login w-100' name="departamento" id="departamento">
                 <option value="" selected disabled>Departamento</option>
-                <option value="opcion1">Opción 1</option>
-                <option value="opcion2">Opción 2</option>
+                {departamentos.map(departamento => (
+                  <option key={departamento.departamento_id} value={departamento.departamento_nombre}>
+                    {departamento.departamento_nombre}
+                  </option>
+                ))}
               </select>
             </div>
             <div className='div-input'>
@@ -61,10 +78,10 @@ export const SignIn = () => {
             <div className='div-input'>
               <textarea className='input-login' name="direccion" id="direccion" cols="30" rows="5" placeholder='Escriba su direccion'></textarea>
             </div>
-           
+
             <div className='w-100 mt-5'>
-                    <button onClick={LogIn} className='btn btn-danger w-100' type='button'>Registrarme</button>
-                </div>
+              <button onClick={LogIn} className='btn btn-danger w-100' type='button'>Registrarme</button>
+            </div>
           </div>
         </div>
       </section>
