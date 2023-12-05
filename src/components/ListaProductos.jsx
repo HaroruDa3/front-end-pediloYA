3a0ff5243e725e7aefd8948d83a37e0c61889c29
HaroruDa3 <harol.kock34@gmail.com>
2023-12-04 23:46:27 -0600

    x
    x

--- src/components/ListaProductos.jsx
+++ src/components/ListaProductos.jsx
@@ -12,9 +12,8 @@ export const ListaProductos = () => {
 
   useEffect(() => {
     const cargarProductos = async () => {
-      const url = "http://localhost:8080/api/v1/productos";
       try {
-        const response = await axios.get(url);
+        const response = await axios.get('http://localhost:8080/api/v1/productos');
         setProductos(response.data);
         const cantidadesInicial = {};
         response.data.forEach(producto => {
@@ -37,25 +36,21 @@ export const ListaProductos = () => {
     mostrarCarrito();
   }, []);
 
-  const aumentarCantidad = (id) => {
-    setCantidades(prevCantidades => ({
-      ...prevCantidades,
-      [id]: prevCantidades[id] + 1,
-    }));
+  const actualizarLocalStorage = (nuevoCarrito) => {
+    localStorage.setItem('carrito', JSON.stringify(nuevoCarrito));
+    setCarritoInfo(nuevoCarrito);
   };
 
-  const disminuirCantidad = (id) => {
-    if (cantidades[id] > 1) {
-      setCantidades(prevCantidades => ({
-        ...prevCantidades,
-        [id]: prevCantidades[id] - 1,
-      }));
-    }
+  const ajustarCantidad = (id, cantidad) => {
+    const nuevasCantidades = { ...cantidades, [id]: cantidad };
+    setCantidades(nuevasCantidades);
   };
 
   const agregarCarrito = (id) => {
-    let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
-    const productoEnCarrito = carrito.find(item => item.id === id);
+    const carritoLocalStorage = JSON.parse(localStorage.getItem('carrito')) || [];
+    const cantidad = cantidades[id];
+
+    const productoEnCarrito = carritoLocalStorage.find(item => item.id === id);
     const cantidadExistente = productoEnCarrito ? productoEnCarrito.cantidad : 0;
     const producto = productos.find(p => p.producto_id === id);
 
@@ -63,39 +58,90 @@ export const ListaProductos = () => {
       id: producto.producto_id,
       nombre: producto.producto_nombre,
       precio: producto.precio,
-      cantidad: cantidadExistente + cantidades[id]
+      cantidad: cantidadExistente + cantidad
     };
 
-    if (productoEnCarrito) {
-      carrito = carrito.map(item => (item.id === id ? nuevoProducto : item));
-    } else {
-      carrito.push(nuevoProducto);
-    }
+    const nuevoCarrito = productoEnCarrito
+      ? carritoLocalStorage.map(item => (item.id === id ? nuevoProducto : item))
+      : [...carritoLocalStorage, nuevoProducto];
 
-    localStorage.setItem('carrito', JSON.stringify(carrito));
-    console.log(`Producto ID: ${id}, Cantidad: ${nuevoProducto.cantidad}`);
-    console.log('Producto seleccionado:', nuevoProducto);
+    actualizarLocalStorage(nuevoCarrito);
 
-    // Actualizar la información del carrito
-    setCarritoInfo(carrito);
-
-      Swal.fire({
-        title: 'Producto agregado al carrito',
-        showConfirmButton: true
-      })
-  };
-
-  const mostrarCarrito = () => {
-    const carritoLocalStorage = JSON.parse(localStorage.getItem('carrito')) || [];
-    setCarritoInfo(carritoLocalStorage);
+    Swal.fire({
+      title: 'Producto agregado al carrito',
+      showConfirmButton: true
+    });
   };
 
   const eliminarProductoCarrito = (id) => {
     const carritoLocalStorage = JSON.parse(localStorage.getItem('carrito')) || [];
     const nuevoCarrito = carritoLocalStorage.filter(item => item.id !== id);
-    localStorage.setItem('carrito', JSON.stringify(nuevoCarrito));
-    setCarritoInfo(nuevoCarrito);
+    actualizarLocalStorage(nuevoCarrito);
   };
+
+  const generarFactura = async () => {
+    const carrito = JSON.parse(localStorage.getItem('carrito'));
+    let detalleProductos = "";
+    let detalle = 0;
+  
+    carrito.forEach(producto => {
+      detalleProductos += ' ' + producto.cantidad + ' ' + producto.nombre;
+      detalle += parseFloat(producto.cantidad * producto.precio);
+    });
+  
+    try {
+      const result = await Swal.fire({
+        title: `Detalle: ${detalleProductos}, Total:${detalle}, ¿Seguro quiere hacer el pedido?`,
+        showDenyButton: true,
+        confirmButtonText: 'Hacer Pedido',
+        denyButtonText: `No`
+      });
+  
+      if (result.isConfirmed) {
+        const dataPedido = {
+          cliente_id: parseInt(localStorage.getItem('id')),
+          total: detalle
+        };
+  
+        const responsePedido = await axios.post('http://localhost:8080/api/v1/pedidos', dataPedido);
+        const pedido_id = responsePedido.data.pedido_id;
+  
+        const pedidosxproductosData = carrito.map(async (producto) => {
+          const pedidoXProductoData = {
+            pedido_id: pedido_id,
+            producto_id: producto.id
+          };
+  
+          try {
+            const response = await axios.post('http://localhost:8080/api/v1/pedidosxproductos', pedidoXProductoData);
+            return response.data; 
+          } catch (error) {
+            console.error(`Error al asociar el producto ID ${producto.id} al pedido:`, error);
+            throw error;  
+          }
+        });
+  
+        await Promise.all(pedidosxproductosData);
+        Swal.fire({
+          title: 'Pedido realizado',
+          showConfirmButton: true
+        }).then((result) => {
+          if (result.isConfirmed) {
+          localStorage.removeItem('carrito')
+          }
+        });
+      } else if (result.isDenied) {
+        Swal.fire('Pedido Cancelado', '', 'info');
+      }
+    } catch (error) {
+      console.error('Error al generar la factura:', error);
+      Swal.fire({
+        title: 'Error al realizar el pedido, inténtelo de nuevo',
+        showConfirmButton: true
+      });
+    }
+  };
+  
 
   return (
     <>
@@ -116,7 +162,7 @@ export const ListaProductos = () => {
                   <p name='precio' className='fw-bold'>Precio: {producto.precio}lps</p>
                   <div className='w-100 d-flex h-5 mb-3'>
                     <div className='w-25 p-1'>
-                      <button type="button" className='btn btn-danger w-100 h-100 d-flex align-items-center justify-content-center' onClick={() => aumentarCantidad(producto.producto_id)}>
+                      <button type="button" className='btn btn-danger w-100 h-100 d-flex align-items-center justify-content-center' onClick={() => ajustarCantidad(producto.producto_id, cantidades[producto.producto_id] + 1)}>
                         <i className="fas fa-plus"></i>
                       </button>
                     </div>
@@ -124,13 +170,13 @@ export const ListaProductos = () => {
                       <input className='w-100 h-100 form-control text-center' type="number" value={cantidades[producto.producto_id]} readOnly />
                     </div>
                     <div className='w-25 p-1'>
-                      <button type="button" className='btn btn-danger w-100 h-100 d-flex align-items-center justify-content-center' onClick={() => disminuirCantidad(producto.producto_id)}>
+                      <button type="button" className='btn btn-danger w-100 h-100 d-flex align-items-center justify-content-center' onClick={() => ajustarCantidad(producto.producto_id, cantidades[producto.producto_id] - 1)}>
                         <i className="fas fa-minus"></i>
                       </button>
                     </div>
                   </div>
                   <div className='w-100 d-flex justify-content-center'>
-                    <button type="button" onClick={() => { agregarCarrito(producto.producto_id); }} className='btn btn-success'>
+                    <button type="button" onClick={() => agregarCarrito(producto.producto_id)} className='btn btn-success'>
                       Agregar al carrito
                     </button>
                   </div>
@@ -141,7 +187,7 @@ export const ListaProductos = () => {
         </div>
 
         <button aria-label="carrito" className="btn btn-flotante" data-bs-toggle="modal" data-bs-target="#carritoModal">
-          <img src={carrito} className="img-btn-flotante" alt=""/>
+          <img src={carrito} className="img-btn-flotante" alt="" />
         </button>
       </div>
 
@@ -182,7 +228,7 @@ export const ListaProductos = () => {
               <div id="totalPagar" className="d-flex align-content-center fw-bold">
                 Total: {carritoInfo.reduce((total, producto) => total + (producto.cantidad * producto.precio), 0).toFixed(2)}lps
               </div>
-              <button type="button" className="btn btn-primary" onClick={mostrarCarrito}>Pagar</button>
+              <button type="button" className="btn btn-primary" onClick={generarFactura} data-bs-dismiss="modal" aria-label="Close">Pagar</button>
             </div>
           </div>
         </div>
