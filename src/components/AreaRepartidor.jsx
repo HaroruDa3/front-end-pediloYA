d39d733f81546b5b8d41e04226d4ab63644368af
HaroruDa3 <harol.kock34@gmail.com>
2023-12-11 15:19:08 -0600

    WASETFGHJKUYTREWQ
    drftghjk

--- src/components/AreaRepartidor.jsx
+++ src/components/AreaRepartidor.jsx
@@ -4,38 +4,57 @@ import { Navbar } from "./Navbar"
 import '../components/css/areaInfo.css';
 
 export const AreaRepartidor = () => {
-const [pedidosAsignados, setPedidos]=useState({})
+  const [pedidosEntregados, setPedidos]=useState({})
+const [pedidosAsignados, setPedidosAsig]=useState({})
 
 const idCliente = parseInt(localStorage.getItem('id'));
 
   useEffect(() => {
-    const pedidosTodos = async () => {
+    const pedidos = async () => {
       try {
-        const url = "http://localhost:8080/api/v1/pedidos";
-        const response = await axios.get(url);
-        let pedidosCliente = response.data.filter(
-          (pedido) => pedido.cliente_id === idCliente
+        const url = "http://localhost:8080/api/v1/pedidos_asignados";
+        const response = (await axios.get(url)).data;
+        const response1 =(await axios.get("http://localhost:8080/api/v1/pedidos-asignados")).data;
+
+        let detallesPedidos=[];
+        let pedidosAsignados = response.filter(
+          (pedido) => pedido.repartidor_id === idCliente
         );
-        setPedidos(pedidosCliente);
+
+        const response3=(await axios.get("http://localhost:8080/api/v1/pedidosxproductos")).data
+        const datos={
+          id_pedido:0,
+          detalle:"",
+          entregado:false,
+          cliente:"",
+          direccion:""
+        }
+        pedidosAsignados.array.forEach(pedido => {
+         datos.id_pedido=pedido.pedido_id;
+         datos.entregado=pedido.entregado;
+          const productos=response3.filter((productos)=>productos.pedido_id == pedido.pedido_id)
+          let detalle=pedido.pedido_id
+          let data= response1.filter((detalle)=>detalle.pedido_id==pedido.pedido_id);
+          data.forEach(pedido=>{
+            const direccion= ((axios.get(`http://localhost:8080/api/v1/colonias/${pedido.direccion}`))).data
+            datos.cliente=detalle.cliente;
+            datos.direccion=direccion.colonia_nombre+", "+direccion.referencia;
+            let producto =productos.find((producto)=>producto.producto_id==pedido.producto_id)
+            detalle+=producto.cantidad+" "+pedido.producto_nombre+" "
+          })
+          datos.detalle=detalle
+          detallesPedidos.push(data)
+        });
+        setPedidosAsig(datos)
+        console.log(pedidosAsignados);
       } catch (error) {
         console.error("Error al obtener pedidos:", error);
       }
     };
 
-    pedidosTodos();
+    pedidos();
   }, []);
 
-const getPedidos = async () =>{
-    const url=`http://localhost:8080/api/v1/pedidos-asignados`
-    const response = await axios.get(url);
-    let data=response.data.find(pedido=>pedido.repartidor_id==localStorage.getItem('id'))
-    let producto=
-    setPedidos(data)
-}
-
-useEffect(()=>{
-    getPedidos();
-},[])
 
   return (
     <>
@@ -52,14 +71,7 @@ useEffect(()=>{
         <div className="w-50">
            <h4 className="mb-5 text-center"> Pedidos Entregados</h4>
            <div className="w-100 d-flex justify-content-center">
-           {pedidosAsignados.map((pedido) => (
-              <div className="cuerpo-card d-flex flex-column w-75" key={pedido.pedido_id}>
-                <h5 className="mb-1">Pedido no. {pedido.pedido_id}</h5>
-                <p className="mb-4">Detalle de pedido: {pedido.detalle}</p>
-                <p className="mb-4">Nombre repartidor: {pedido.repartidor}</p>
-                <p>Total: {pedido.total}lps</p>
-              </div>
-            ))}
+         
             </div>
         </div>
     </section>
